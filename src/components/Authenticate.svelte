7c1f9fa1c8b0fec6045b82cabb3638cf68e9906a
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-27 23:17:12 -0500

    creating authentication inputs

---
+++ src/components/Authenticate.svelte
@@ -0,0 +1,15 @@
+<div class="authContainer">
+    <form>
+        <h1>Login</h1>
+        <label>
+            <input type="email" placeholder="email"/>
+        </label>
+        <label>
+            <input type="email" placeholder="email"/>
+        </label>
+        <label>
+            <input type="email" placeholder="email"/>
+        </label>
+    </form>
+</div>
+
